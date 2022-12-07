b1e8d38f0b4685515a1821dd9bb891bb2a024da9
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2022-12-06 23:37:10 -0300

    Hola Mundo

---
+++ Pagina_Flask/app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)  #Inicializa aplicacion.
+@app.route('/')
+def hello_world():
+    #bienvenida = "Bienvenido a el repositorio,"
+    #return bienvenida
+    return render_template('index.html')
+
+if __name__ == '__main__':  #Comprobar que estemos situados en el archivo principal.
+    app.run(debug=True)
