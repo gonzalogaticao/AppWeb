257f5829f2b64b6554275b1c3b5e3d14893ffc57
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2022-12-12 18:23:37 -0300

    Base + html

--- Pagina_Flask/app/app.py
+++ Pagina_Flask/app/app.py
@@ -3,10 +3,16 @@ from flask import render_template
 
 app = Flask(__name__)  #Inicializa aplicacion.
 @app.route('/')
-def hello_world():
-    #bienvenida = "Hola Mundo!"
-    #return bienvenida
+def index():
     return render_template('index.html')
+
+@app.route('/tesis')
+def tesis():
+    return render_template('tesis.html')
+
+@app.route('/envio')
+def envio():
+    return render_template('envio.html')
 
 if __name__ == '__main__':  #Comprobar que estemos situados en el archivo principal.
     app.run(debug=True)
