80de9537764ace2f91591ce1e4826f666a91f60e
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2022-12-13 17:39:37 -0300

    Admin, Recibe informacion, No hay conexion a BD

--- Pagina_Flask/app/app.py
+++ Pagina_Flask/app/app.py
@@ -1,18 +1,37 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)  #Inicializa aplicacion.
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('site/index.html')
 
 @app.route('/tesis')
 def tesis():
-    return render_template('tesis.html')
+    return render_template('site/tesis.html')
 
 @app.route('/envio')
 def envio():
-    return render_template('envio.html')
+    return render_template('site/envio.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin/index.html')
+
+@app.route('/admin/login')
+def login():
+    return render_template('admin/login.html')
+
+@app.route('/admin/tesis')
+def adminTesis():
+    return render_template('admin/tesis.html')
+
+@app.route('/admin/tesis/save', methods=['POST'])
+def atesisSave():
+    print(request.form['txtTitulo'])
+    return redirect('admin/tesis')
 
 if __name__ == '__main__':  #Comprobar que estemos situados en el archivo principal.
     app.run(debug=True)
